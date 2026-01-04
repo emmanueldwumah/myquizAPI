48753b5f4b0f25ba9bffebd906107a241ea6c9c8
Ci$c0 <emmanueloduro37@gmail.com>
2026-01-04 21:48:03 +0000

    Configured views and urls for Categories Endpoint

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Quiz, Choice, Attempt, Categories
+from .serializers import QuizSerializer, CategoriesSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+class CategoriesListView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        categories = Categories.objects.all()
+        serializer = CategoriesSerializer(categories, many=True)
+
+        return Response(serializer.data)
