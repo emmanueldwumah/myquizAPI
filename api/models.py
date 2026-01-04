99299ca543696d8b9b140f2888e40819f391e5a5
Ci$c0 <emmanueloduro37@gmail.com>
2026-01-04 19:42:36 +0000

    Created Categories and Question models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+    
 class Quiz(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
@@ -8,4 +15,13 @@ class Quiz(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, related_name='questions', on_delete=models.CASCADE)
+    category = models.ForeignKey(Categories, related_name='questions', on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.text
+    
     
