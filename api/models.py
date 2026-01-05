4c88549cbe576694f47b6ac317043d48b0a75289
Ci$c0 <emmanueloduro37@gmail.com>
2026-01-05 09:59:57 +0000

    Fixed plural name for Category

--- api/models.py
+++ api/models.py
@@ -10,6 +10,15 @@ class Categories(models.Model):
     name = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(unique=True)
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    
     def __str__(self):
         return self.name
     
